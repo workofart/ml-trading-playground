added normalization to data and other minor fixes

playground/dqn/dqn.py
@@ -7,9 +7,10 @@ from playground.env.trading_env import TradingEnv
 EPISODE = 56 # Episode limitation
 TEST = 3 # The number of experiment test every 1 episode, for reducing variance
 TRAIN_EVERY_STEPS = 1
+TEST_EVERY_EP = 5
 BATCH_SIZE = 64 # size of minibatch
-TEST_STEPS = 100
-DATA_LENGTH = 9000 # How many times steps to use in the data
+TEST_STEPS = 4999
+DATA_LENGTH = 5000 # How many times steps to use in the data
 
 def main():
     env = TradingEnv(data_length = DATA_LENGTH)
@@ -37,36 +38,37 @@ def main():
         cost_plot_data.append(np.mean(cost_per_episode))
 
         # After one full timestep (timestep to end) is completed, test for TEST rounds
-        avg_reward_list = []
-        for t in range(TEST):
-            state = agent.env.reset()
-            random_time_step = random.randint(0, DATA_LENGTH - TEST_STEPS - 1)
-            agent.env.time_step = random_time_step
-            avg_reward = 0
-            # total_reward = 0
-            done = False
-            # while done is False:
-            for step in range(TEST_STEPS):
-                action = agent.act(state)
-                state, reward, done, _ = agent.env.step(action)
-            avg_reward = agent.env.episode_total_reward / TEST
-            avg_reward_list.append(avg_reward)
+        if i % TEST_EVERY_EP == 0:
+            avg_reward_list = []
+            for t in range(TEST):
+                state = agent.env.reset()
+                random_time_step = random.randint(0, DATA_LENGTH - TEST_STEPS - 1)
+                agent.env.time_step = random_time_step
+                avg_reward = 0
+                # total_reward = 0
+                done = False
+                # while done is False:
+                for step in range(TEST_STEPS):
+                    action = agent.act(state)
+                    state, reward, done, _ = agent.env.step(action)
+                avg_reward = agent.env.episode_total_reward / TEST
+                avg_reward_list.append(avg_reward)
 
-        print('Avg Reward: %.8f' % (np.mean(avg_reward_list) * 1000))
-        reward_plot_data.append(np.mean(avg_reward_list))
-        plot_reward_cost(reward_plot_data, cost_plot_data)
+            print('Avg Reward: %.8f' % (np.mean(avg_reward_list)))
+            reward_plot_data.append(np.mean(avg_reward_list))
+            plot_reward_cost(reward_plot_data, cost_plot_data)
     
     # plot_reward(reward_plot_data)
 
 
 def test():
-    env = TradingEnv(DATA_LENGTH / 4)
+    env = TradingEnv(int(DATA_LENGTH / 4))
     agent = DQN_Agent(env, False)
     state = agent.env.reset() # To start the process
 
     prices = []
     actions = []
-    for i in range(DATA_LENGTH / 4):
+    for i in range(int(DATA_LENGTH / 4)):
         prices.append(state[0][2])
         action = agent.act(state)
         actions.append(action)

playground/utilities/utils.py
@@ -12,6 +12,8 @@ def read_data(filename, ticker):
     data['timestamp'] = pd.to_datetime(data.timestamp)
     data = data[['high', 'low', 'price', 'volume', 'timestamp']].sort_values(by='timestamp')
     data = data.set_index('timestamp')
+
+    data = norm2(data.values, axis=0)
     return data
 
 def normalize(data):
@@ -82,8 +84,8 @@ def plot_trades(prices, actions):
             # plt.annotate('x', xy=(i, prices[i]), color='red')
         # elif action == 2:
             # plt.annotate('x', xy=(i, np.mean(prices)), color='yellow')
-    plt.plot(buys['x'], buys['y'], '^', markersize=10, color='g')
-    plt.plot(sells['x'], sells['y'], 'v', markersize=10, color='r')
+    plt.plot(buys['x'], buys['y'], '^', markersize=1, color='g')
+    plt.plot(sells['x'], sells['y'], 'v', markersize=1, color='r')
     plt.ylabel('Prices')
     plt.xlabel('Timesteps')
     plt.show()