wip fix for exploding/vanishing gradient

playground/a3c_new/ac_network.py
@@ -49,10 +49,12 @@ class AC_Network():
             self.policy = tf.layers.dense(l2,action_N,
                 activation=tf.nn.softmax,
                 kernel_initializer=normalized_columns_initializer(0.01),
+                # kernel_initializer=w_init,
                 bias_initializer=None)
             self.value = tf.layers.dense(l2,1,
                 activation=None,
                 kernel_initializer=normalized_columns_initializer(1.0),
+                # kernel_initializer=w_init,
                 bias_initializer=None)
             
             #Only the worker network need ops for loss functions and gradient updating.
@@ -74,7 +76,7 @@ class AC_Network():
                 local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
                 self.gradients = tf.gradients(self.loss,local_vars)
                 self.var_norms = tf.global_norm(local_vars)
-                grads,self.grad_norms = tf.clip_by_global_norm(self.gradients,40.0)
+                grads,self.grad_norms = tf.clip_by_global_norm(self.gradients,0.5)
                 
                 #Apply local gradients to global network
                 global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')

playground/a3c_new/worker.py
@@ -40,20 +40,20 @@ class Worker():
     # [state, actions, rewards, next_state, values]
     def train(self,rollout,sess,gamma,bootstrap_value):
         rollout = np.array(rollout.buffer)
-        states = rollout[:,0]
-        actions = rollout[:,1]
-        rewards = rollout[:,2]
-        next_states = rollout[:,3]
-        values = rollout[:,5]
+        states = [i.astype(np.float32) for i in rollout[:,0]]
+        actions = rollout[:,1].astype(np.int32)
+        rewards = rollout[:,2].astype(np.float32)
+        next_states = [i.astype(np.float32) for i in rollout[:,3]]
+        values = rollout[:,5].astype(np.float32)
         
         # Here we take the rewards and values from the rollout, and use them to 
         # generate the advantage and discounted returns. 
         # The advantage function uses "Generalized Advantage Estimation"
         self.rewards_plus = np.asarray(rewards.tolist() + [bootstrap_value])
-        discounted_rewards = discount(self.rewards_plus,gamma)[:-1]
+        discounted_rewards = discount(self.rewards_plus,gamma)[:-1].astype(np.float32)
         self.value_plus = np.asarray(values.tolist() + [bootstrap_value])
         advantages = rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]
-        advantages = discount(advantages,gamma)
+        advantages = discount(advantages,gamma).astype(np.float32)
 
         # Update the global network using gradients from loss
         # Generate network statistics to periodically save
@@ -61,14 +61,17 @@ class Worker():
             self.local_AC.state:np.vstack(states),
             self.local_AC.actions:actions,
             self.local_AC.advantages:advantages}
-        v_l,p_l,e_l,g_n,v_n,_ = sess.run([self.local_AC.value_loss,
+        v_l,p_l,e_l,g_n,v_n,_, gradients= sess.run([self.local_AC.value_loss,
             self.local_AC.policy_loss,
             self.local_AC.entropy,
             self.local_AC.grad_norms,
             self.local_AC.var_norms,
             # self.local_AC.state_out,
-            self.local_AC.apply_grads],
+            self.local_AC.apply_grads,
+            self.local_AC.gradients],
             feed_dict=feed_dict)
+        print(min([i.min() for i in gradients]))
+        print(max([i.max() for i in gradients]))
         return v_l / len(rollout),p_l / len(rollout),e_l / len(rollout), g_n,v_n
         
     def work(self,max_episode_length,gamma,sess,coord,saver):
@@ -98,7 +101,7 @@ class Worker():
                     else:
                         s1 = s
                         
-                    experience_buffer.add([s,a,r,s1,d,v[0,0]])
+                    experience_buffer.add([s[0],a,r,s1[0],d,v[0,0]])
                     episode_values.append(v[0,0])
 
                     episode_reward += r

playground/env/trading_env.py
@@ -60,9 +60,9 @@ class TradingEnv():
 
     def _get_obs(self, pos=None):
         if pos is None:
-            return np.reshape(self.observation_space[self.time_step], (1, 4))
+            return np.vstack(self.observation_space[self.time_step]).T            
         else:
-            return np.reshape(self.observation_space[pos], (1, 4))
+            return np.vstack(self.observation_space[pos]).T
 
     def process_action(self, action, state):
         cur_price = state[0][2]