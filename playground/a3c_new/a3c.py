fixed exploding gradient problem, but still no reward

playground/a3c_new/a3c.py
@@ -28,7 +28,7 @@ if not os.path.exists(model_path):
 
 with tf.device("/cpu:0"): 
     global_episodes = tf.Variable(0,dtype=tf.int32,name='global_episodes',trainable=False)
-    trainer = tf.train.AdamOptimizer(learning_rate=1e-8)
+    trainer = tf.train.AdamOptimizer(learning_rate=1e-6)
     master_network = AC_Network(s_size,a_size,'global',None) # Generate global network
     num_workers = multiprocessing.cpu_count() # Set workers to number of available CPU threads
     workers = []

playground/a3c_new/ac_network.py
@@ -39,19 +39,31 @@ class AC_Network():
 
             w_init = tf.random_normal_initializer(0., .1)
             with tf.variable_scope('actor'):
-                l1 = tf.layers.dense(self.state, NN1_NEURONS, tf.nn.relu, kernel_initializer=w_init, name='layer_actor1')
-                l2 = tf.layers.dense(l1, NN2_NEURONS, tf.nn.relu, kernel_initializer=w_init, name='layer_actor2')
+                l1 = tf.layers.dense(self.state,
+                                    NN1_NEURONS, 
+                                    tf.nn.relu, 
+                                    kernel_initializer=w_init,
+                                    bias_initializer=tf.constant_initializer(0.1),
+                                    name='layer_actor1')
+                l2 = tf.layers.dense(l1, 
+                                    NN2_NEURONS, 
+                                    tf.nn.relu, 
+                                    kernel_initializer=w_init, 
+                                    bias_initializer=tf.constant_initializer(0.1),
+                                    name='layer_actor2')
             # a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
             # c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
 
             
             #Output layers for policy and value estimations
-            self.policy = tf.layers.dense(l2,action_N,
+            self.policy = tf.layers.dense(l2,
+                action_N,
                 activation=tf.nn.softmax,
                 kernel_initializer=normalized_columns_initializer(0.01),
                 # kernel_initializer=w_init,
                 bias_initializer=None)
-            self.value = tf.layers.dense(l2,1,
+            self.value = tf.layers.dense(l2,
+                1,
                 activation=None,
                 kernel_initializer=normalized_columns_initializer(1.0),
                 # kernel_initializer=w_init,